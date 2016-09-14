681118024d4dd5b1b0449d9c7df2332011d01da2
ANDREA <andrea.palomino.ruby@gmail.com>
2016-09-14 12:00:33 -0700

    new rep

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML = "Por favor Iniciar sesion" ;
+document.getElementById("inputEmail").placeholder="Correo electronico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector('span').innerHTML = "Recordar Datos";
+document.querySelector('.btn').innerHTML ="INICIAR SESION";
